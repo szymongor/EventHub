1a644be2e379556921f7b9eecd696d6a1208cd6c
szymongor <gornioczek.szymon@gmail.com>
2018-05-15 12:24:32 +0200

    Middleware - Form validation flow

--- client/src/actions/form_actions.js
+++ client/src/actions/form_actions.js
@@ -1,10 +1,32 @@
 export const FORM_SUBMIT = 'FORM_SUBMIT';
+export const VALID_FORM_SUBMIT = 'VALID_FORM_SUBMIT';
+export const INVALID_FORM_SUBMIT = 'INVALID_FORM_SUBMIT';
 
 export function formSubmit(formFields) {
   return {
     type: FORM_SUBMIT,
     payload: {
       formFields
+    }
+  };
+}
+
+export function validFormSubmit(formFields) {
+  console.log(formFields);
+  return {
+    type: VALID_FORM_SUBMIT,
+    payload: {
+      formFields
+    }
+  };
+}
+
+export function invalidFormSubmit(status) {
+  console.log(status);
+  return {
+    type: INVALID_FORM_SUBMIT,
+    payload: {
+      status
     }
   };
 }
--- client/src/middleware/process_form.js
+++ client/src/middleware/process_form.js
@@ -1,13 +1,53 @@
-import { FORM_SUBMIT } from '../actions/form_actions';
+import {
+  validFormSubmit,
+  invalidFormSubmit,
+  FORM_SUBMIT
+} from '../actions/form_actions';
 
-const formValidation = store => next => action => {
+const normalizeForm = store => next => action => {
   if (action.type === FORM_SUBMIT) {
-    console.log(action.payload);
+    let formFields = action.payload.formFields;
+    let formData = {
+      firstName: formFields.firstName,
+      lastName: formFields.lastName,
+      email: formFields.email,
+      eventDate: formFields.startDate.unix()
+    };
+    action.payload.data = formData;
   }
   let result = next(action);
 
   return result;
 };
 
-const processFormMDL = [formValidation];
+const formValidation = store => next => action => {
+  if (action.type === FORM_SUBMIT) {
+    let status = { validForm: true };
+    let formFields = action.payload.data;
+    for (let field in formFields) {
+      if (formFields[field] === undefined || formFields[field] === '') {
+        status.validForm = false;
+      }
+    }
+    action.payload.status = status;
+  }
+  let result = next(action);
+
+  return result;
+};
+
+const actionRouter = store => next => action => {
+  if (action.type === FORM_SUBMIT) {
+    if (action.payload.status.validForm) {
+      store.dispatch(validFormSubmit(action.payload.data));
+    } else {
+      store.dispatch(invalidFormSubmit(action.payload.status));
+    }
+  }
+  let result = next(action);
+
+  return result;
+};
+
+const processFormMDL = [normalizeForm, formValidation, actionRouter];
 export default processFormMDL;
